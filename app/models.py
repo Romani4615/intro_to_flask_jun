810da68fd0869489553b47aaf7ceb553fccf7c90
Brian <bstanton773@gmail.com>
2021-08-20 10:39:57 -0500

    Add token and token_exp to User

--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
+import os
+import base64
 from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
-from datetime import datetime
+from datetime import datetime, timedelta
 from flask_login import UserMixin
 
 
@@ -14,6 +16,8 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(50), nullable=False, unique=True)
     password = db.Column(db.String(256), nullable=False)
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    token = db.Column(db.String(32), index=True, unique=True)
+    token_expiration = db.Column(db.DateTime)
 
     def __init__(self, username, email, password):
         self.username = username
@@ -30,6 +34,24 @@ class User(db.Model, UserMixin):
             'email': self.email
         }
 
+    def get_token(self, expires_in=3600):
+        now = datetime.utcnow()
+        if self.token and self.token_expiration > now + timedelta(seconds=60):
+            return self.token
+        self.token = base64.b64encode(os.urandom(24)).decode('utf-8')
+        self.token_expiration = now + timedelta(seconds=expires_in)
+        db.session.add(self)
+        return self.token
+
+    def revoke_token(self):
+        self.token_expiration = datetime.utcnow() - timedelta(seconds=1)
+
+    @classmethod
+    def check_token(cls, token):
+        user = cls.query.filter_by(token=token).first()
+        if user is None or user.token_expiration < datetime.utcnow():
+            return None
+        return user
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
